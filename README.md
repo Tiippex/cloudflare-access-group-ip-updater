474e377d278b331f4dabd7b56fe1f29bfc3f6c3f
Tiippex <16509128+Tiippex@users.noreply.github.com>
2025-07-11 17:01:53 +0200

    Breaking Change: Cloudflare deprecated App-Groups. Now you should use Access Policies

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Cloudflare Zero Trust Access Group IP Updater
+# Cloudflare Zero Trust Access Policy IP Updater
 
-This Docker container fetches the public IP address of the host it is running on and updates a Cloudflare Zero Trust Access Group by adding the current public IP to the group. It also accepts a comma-separated list of additional IPs to whitelist, and can resolve DNS records to IP addresses. The updates are made at a configurable interval.
+This Docker container fetches the public IP address of the host it is running on and updates a Cloudflare Zero Trust Access Policies by adding the current public IP to the Policy. It also accepts a comma-separated list of additional IPs to whitelist, and can resolve DNS records to IP addresses. The updates are made at a configurable interval.
 
 ## Features:
 - Fetches the current public IP address from `https://ip.tiippex.de`.
-- Adds the current public IP to Cloudflare Zero Trust's Access Group.
+- Adds the current public IP to Cloudflare Zero Trust's Access Polices.
 - Accepts a list of IP addresses or DNS records to whitelist alongside the public IP.
 - Runs at a user-defined interval (in minutes), or runs once and exits if no interval is specified.
 - Optional IP lookup and DNS resolution for more flexibility.
@@ -12,15 +12,15 @@ This Docker container fetches the public IP address of the host it is running on
 ## API Key Permissions:
 
 - When creating the API token in Cloudflare, set the following permission for the token:
-    - **Account** > **Access: Organizations, Identity Providers, and Groups** > **Edit**.
+    - **Account** > **Access: Apps and Policies** > **Edit**.
 
 ## Environment Variables:
 
-| Environment Variable         | Description                                                                              | Required  | Default   |
-|------------------------------|------------------------------------------------------------------------------------------|-----------|-----------|
+| Environment Variable          | Description                                                                              | Required  | Default   |
+|-------------------------------|------------------------------------------------------------------------------------------|-----------|-----------|
 | **`CLOUDFLARE_API_KEY`**      | Your Cloudflare API key.                                                                 | Yes       | None      |
 | **`CLOUDFLARE_ACCOUNT_ID`**   | The Account ID from your Cloudflare dashboard.                                           | Yes       | None      |
-| **`CLOUDFLARE_GROUP_ID`**     | The Access Group ID from your Zero Trust dashboard.                                      | Yes       | None      |
+| **`CLOUDFLARE_POLICY_ID`**    | The Access Policy ID from your Zero Trust dashboard.                                     | Yes       | None      |
 | **`IP_RANGE`**                | A comma-separated list of IPs to whitelist                                               | No        | None      |
 | **`IP_LOOKUP_ENABLED`**       | Set to `true` (default) or `false` to control whether the public IP lookup is performed. | No        | `true`    |
 | **`IP_FROM_DNS`**             | A comma-separated list of DNS records to resolve to IPs and whitelist.                   | No        | None      |
@@ -34,12 +34,12 @@ To run the Docker container with necessary environment variables:
 docker run -e TZ=Europe/Berlin \
            -e CLOUDFLARE_API_KEY=your_api_key \
            -e CLOUDFLARE_ACCOUNT_ID=your_account_id \
-           -e CLOUDFLARE_GROUP_ID=your_group_id \
+           -e CLOUDFLARE_POLICY_ID=your_policy_id \
            -e IP_RANGE="127.0.0.1,10.0.0.0/24" \
            -e IP_LOOKUP_ENABLED=true \
            -e IP_FROM_DNS="example.com,github.com" \
            -e UPDATE_INTERVAL_MINUTES=15 \
-           tiippex/cloudflare-access-group-ip-updater:latest
+           tiippex/cloudflare-access-policy-ip-updater:latest
 ```
 
 ## Docker Compose Example:
@@ -49,13 +49,13 @@ Below is an example `docker-compose.yml` file that you can use to set up the con
 ```yaml
 version: "3"
 services:
-  cloudflare-access-group-updater:
-    image: tiippex/cloudflare-access-group-ip-updater:latest
+  cloudflare-access-policy-updater:
+    image: tiippex/cloudflare-access-policy-ip-updater:latest
     environment:
       - TZ=Europe/Berlin
       - CLOUDFLARE_API_KEY=your_api_key
       - CLOUDFLARE_ACCOUNT_ID=your_account_id
-      - CLOUDFLARE_GROUP_ID=your_group_id
+      - CLOUDFLARE_POLICY_ID=your_policy_id
       - IP_RANGE=127.0.0.1,10.0.0.0/24
       - IP_LOOKUP_ENABLED=true
       - IP_FROM_DNS=example.com,github.com
--- updateCloudflareIp.py
+++ updateCloudflareIp.py
@@ -21,8 +21,8 @@ def get_public_ip():
     else:
         raise Exception("Could not get public IP")
 
-def get_access_group(api_key, account_id, group_id):
-    url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/access/groups/{group_id}"
+def get_access_policy(api_key, account_id, policy_id):
+    url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/access/policies/{policy_id}"
     headers = {
         "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json"
@@ -33,7 +33,7 @@ def get_access_group(api_key, account_id, group_id):
     if response.status_code == 200:
         return response.json()['result']
     else:
-        raise Exception(f"Failed to fetch Access Group. Response: {response.text}")
+        raise Exception(f"Failed to fetch Access Policy. Response: {response.text}")
 
 def resolve_dns_to_ip(dns_record):
     try:
@@ -41,8 +41,8 @@ def resolve_dns_to_ip(dns_record):
     except socket.gaierror as e:
         raise Exception(f"Failed to resolve DNS: {dns_record} - {e}")
 
-def update_cloudflare_access_group(api_key, account_id, group_id, ip_range, ip_lookup_enabled, dns_list):
-    url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/access/groups/{group_id}"
+def update_cloudflare_access_policy(api_key, account_id, policy_id, ip_range, ip_lookup_enabled, dns_list):
+    url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/access/policies/{policy_id}"
     headers = {
         "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json"
@@ -63,24 +63,24 @@ def update_cloudflare_access_group(api_key, account_id, group_id, ip_range, ip_l
             except Exception as e:
                 log(f"Error resolving DNS {dns_record}: {e}")
 
-    group_data  = get_access_group(api_key, account_id, group_id)
+    policy_data  = get_access_policy(api_key, account_id, policy_id)
     ip_includes = [{"ip": {"ip": ip}} for ip in ip_range]
 
-    group_data['include'] = ip_includes
+    policy_data['include'] = ip_includes
 
-    log(f"Trying to update {account_id}'s Access Group with IPs: {', '.join(ip_range)}")
+    log(f"Trying to update {account_id}'s Access Policy with IPs: {', '.join(ip_range)}")
 
-    response = requests.put(url, json=group_data, headers=headers)
+    response = requests.put(url, json=policy_data, headers=headers)
 
     if response.status_code == 200:
-        log(f"Successfully updated Access Group with IPs: {', '.join(ip_range)}")
+        log(f"Successfully updated Access Policy with IPs: {', '.join(ip_range)}")
     else:
-        log(f"Failed to update Access Group. Response: {response.text}")
+        log(f"Failed to update Access Policy. Response: {response.text}")
 
 if __name__ == "__main__":
     CLOUDFLARE_API_KEY = os.getenv("CLOUDFLARE_API_KEY")
     CLOUDFLARE_ACCOUNT_ID = os.getenv("CLOUDFLARE_ACCOUNT_ID")
-    CLOUDFLARE_GROUP_ID = os.getenv("CLOUDFLARE_GROUP_ID")
+    CLOUDFLARE_POLICY_ID = os.getenv("CLOUDFLARE_POLICY_ID")
 
     IP_RANGE = os.getenv("IP_RANGE", "").split(",") if os.getenv("IP_RANGE") else []
     IP_FROM_DNS = os.getenv("IP_FROM_DNS", "").split(",") if os.getenv("IP_FROM_DNS") else []
@@ -88,14 +88,14 @@ if __name__ == "__main__":
 
     UPDATE_INTERVAL_MINUTES = os.getenv("UPDATE_INTERVAL_MINUTES")
 
-    if not CLOUDFLARE_API_KEY or not CLOUDFLARE_ACCOUNT_ID or not CLOUDFLARE_GROUP_ID:
-        log("Required environment variables: CLOUDFLARE_API_KEY, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_GROUP_ID")
+    if not CLOUDFLARE_API_KEY or not CLOUDFLARE_ACCOUNT_ID or not CLOUDFLARE_POLICY_ID:
+        log("Required environment variables: CLOUDFLARE_API_KEY, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_POLICY_ID")
         sys.exit(1)
 
     try:
         if not UPDATE_INTERVAL_MINUTES:
             log("No UPDATE_INTERVAL_MINUTES set, running once...")
-            update_cloudflare_access_group(CLOUDFLARE_API_KEY, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_GROUP_ID, IP_RANGE, IP_LOOKUP_ENABLED, IP_FROM_DNS)
+            update_cloudflare_access_policy(CLOUDFLARE_API_KEY, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_POLICY_ID, IP_RANGE, IP_LOOKUP_ENABLED, IP_FROM_DNS)
             log("Execution complete. Exiting.")
             sys.exit(0)
 
@@ -103,7 +103,7 @@ if __name__ == "__main__":
             UPDATE_INTERVAL_MINUTES = int(UPDATE_INTERVAL_MINUTES)
             while True:
                 try:
-                    update_cloudflare_access_group(CLOUDFLARE_API_KEY, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_GROUP_ID, IP_RANGE, IP_LOOKUP_ENABLED, IP_FROM_DNS)
+                    update_cloudflare_access_policy(CLOUDFLARE_API_KEY, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_POLICY_ID, IP_RANGE, IP_LOOKUP_ENABLED, IP_FROM_DNS)
                 except Exception as e:
                     log(f"Error updating Cloudflare: {e}")
 
